54d8278b7725f3ba40bb576ac5f0177660b263fd
Borja González Seoane <borja.gseoane@udc.es>
2021-01-26 20:44:39 +0100

    Add skeleton of penetrance table write function

--- pytoxo/ptable.py
+++ pytoxo/ptable.py
@@ -12,6 +12,7 @@
 ###########################################################
 
 """Penetrance table definition."""
+import os
 
 import sympy
 
@@ -55,3 +56,50 @@ class PTable:
         self._penetrance_values = sympy.substitution(
             model_penetrances, model_variables, values
         )
+
+    def write_to_file(
+        self, filename: str, overwrite: bool = False, format: str = "csv"
+    ) -> None:
+        """Write the penetrance table into a file.
+
+        Currently only CSV format is supported.
+
+        Parameters
+        ----------
+        filename : str
+            The full file name where writes the table, without extension.
+        overwrite : bool (default False)
+            A flag that should be passed as true to overwrite the final file
+            if it already exists.
+        format : str
+            The format of the final file. Currently only CSV format (`csv`
+            flag) is supported.
+
+        Raises
+        ------
+        ValueError
+            If unsupported format tentative.
+        FileExistsError
+            If the `filename` file already exists and the `overwrite` has not be
+            passed as true.
+        IsADirectoryError
+            If `filename` is a existent directory.
+        """
+        # Input handling and checks
+        if format == "CSV":  # Deference fix
+            format = "csv"
+        # Calculate extension
+        if format == "csv":
+            extension = ".csv"
+        else:
+            ValueError(f"Unsupported '{format}' format")
+        # Calculate final filename
+        filename = os.path.normpath(filename + extension)
+        # Check final file name
+        if os.path.exists(filename) and not overwrite:
+            raise FileExistsError(filename)
+        if os.path.exists(filename) and not os.path.isfile(filename):
+            raise IsADirectoryError(filename)
+
+        with open(filename) as f:
+            raise NotImplementedError  # TODO: Implement
