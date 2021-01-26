a60585008ea7fd415758371a2b6c7801e4cb85e4
Borja González Seoane <borja.gseoane@udc.es>
2021-01-26 17:49:35 +0100

    Avoid cyclic imports between 'Model' and 'PTable'
    Now the 'PTable' objects are created using 'Model' attributes and not
    the entire object.

--- pytoxo/ptable.py
+++ pytoxo/ptable.py
@@ -15,8 +15,6 @@
 
 import sympy
 
-import pytoxo.model
-
 
 class PTable:
     """Representation of a penetrance table."""
@@ -25,22 +23,35 @@ class PTable:
     _variables = []  # Values for the variables present in the original model
     _penetrance_values = []  # Array of symbolic penetrances values
 
-    def __init__(self, model: pytoxo.model.Model, values: [int]):
-        """Creates a penetrance table from a given PyToxo model and its
-        variable values.
+    def __init__(
+        self,
+        model_order: int,
+        model_variables: list[sympy.Symbol],
+        model_penetrances: list[float],
+        values: [int],
+    ):
+        """Creates a penetrance table from a given PyToxo model defined by
+        its variables and penetrances, and its variable values.
 
         Parameters
         ----------
-        model : Model
-            Model from which to create the penetrance table.
+        model_order : int
+            Order from the PyToxo model from which to create the penetrance
+            table.
+        model_variables : list[sympy.Symbol]
+            Variables from the PyToxo model from which to create the penetrance
+            table.
+        model_penetrances : list[float]
+            Penetrances from the PyToxo model from which to create the
+            penetrance table.
         values : [int]
              Value for each of the variables represented in model.
         """
-        self._order = model.order
+        self._order = model_order
         self._variables = {
-            str(model.variables[0]): values[0],
-            str(model.variables[1]): values[1],
+            str(model_variables[0]): values[0],
+            str(model_variables[1]): values[1],
         }
         self._penetrance_values = sympy.substitution(
-            model.penetrances, model.variables, values
+            model_penetrances, model_variables, values
         )
