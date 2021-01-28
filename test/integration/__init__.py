89ab9da4ab89fa62b4bff543b3a0135d1a51b271
Borja González Seoane <borja.gseoane@udc.es>
2021-01-28 19:20:45 +0100

    Create integration test package

---
+++ test/integration/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+# PyToxo
+#
+# A Python library for calculating penetrance tables of any
+# bivariate epistasis model.
+#
+# Copyright 2021 Borja González Seoane
+#
+# Contact: borja.gseoane@udc.es
+###########################################################
