97a43b265e6ef0f1b2c4b29ccc5c2d2e31ae95f5
Borja González Seoane <borja.gseoane@udc.es>
2021-02-02 17:08:03 +0100

    Add some helper scripts to easy run tests

---
+++ test/run_all_tests.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+###########################################################
+# PyToxo
+#
+# A Python library for calculating penetrance tables of any
+# bivariate epistasis model.
+#
+# Copyright 2021 Borja González Seoane
+#
+# Contact: borja.gseoane@udc.es
+###########################################################
+
+"""Run all tests helper script.
+
+Simple script to discover and run all tests inside the current directory
+with the project home directory as working directory."""
+
+import os
+import unittest
+
+# Assert project home directory because tests have routes since here
+if os.path.basename(os.getcwd()) == "test":
+    os.chdir(os.pardir)
+
+# Load and run the tests
+loader = unittest.TestLoader()
+tests = loader.discover(os.getcwd(), top_level_dir=os.getcwd())
+testRunner = unittest.runner.TextTestRunner()
+testRunner.run(tests)
