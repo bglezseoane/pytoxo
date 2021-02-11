5e6ec70da6d76da3cc0d2b55e7827a5c5887f18f
Borja González Seoane <borja.gseoane@udc.es>
2021-02-11 18:36:17 +0100

    Merge penetrance accuracy tests in generalized one

--- test/accuracy/test_penetrances_accuracy.py
+++ test/accuracy/test_penetrances_accuracy.py
@@ -14,10 +14,9 @@
 """Part of the PyToxo accuracy test suite.
 
 Script to check PyToxo calculated penetrances accuracy and computation
-time for different models.
-
-These scripts run some automatic checks and generate tables as Latex format to
-revise manually the accuracies and times over different PyToxo versions.
+time for different models. This scripts run some automatic checks and
+generate tables as Latex format to revise manually the accuracies and times
+over different PyToxo versions.
 """
 
 import datetime
@@ -38,15 +37,18 @@ _TOLERABLE_ACCURACY_DELTA = 1e-15  # Maximum error
 _TEST_REPETITIONS = 5  # To confirm computation times with an average
 
 
-class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
+class PenetrancesAccuracyTestSuite(unittest.TestCase):
     """Test suite to check PyToxo calculated penetrances accuracy and
     computation time for different models. I.e. the final penetrances of the
     penetrances tables.
 
-    This test maximizes prevalence.
+    This test is generalized to easily edit some values and achieve
+    different reports. It is possible to maximize both prevalence and
+    heritability. Revise `EDIT HERE` section of the code.
     """
 
     def test_penetrances_accuracy(self):
+        # ####################### EDIT HERE #######################
         models = [
             # "additive_2",
             "additive_3",
@@ -71,14 +73,25 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
             # "threshold_8",
         ]  # Uncomment ones to use in the test
         mafs = [0.1, 0.4]
-        heritabilities = [0.1, 0.8]
+        prevs_or_hers = [0.1, 0.8]  # Prevalences or heritabilities
+
+        # Comment or uncomment firsts or seconds of each pair
+        prev_or_her_str = "Prevalence"
+        # prev_or_her_str = "Heritability"
+        table_method = pytoxo.model.Model.find_max_prevalence_table
+        # table_method = pytoxo.model.Model.find_max_heritability_table
+        recalc_method = pytoxo.calculations.compute_heritability
+        # recalc_method = pytoxo.calculations.compute_prevalence
+        prev_or_her_letter = "p"
+        # prev_or_her_letter = "h"
+        # #########################################################
 
         # Latex table report content
         table_headers = [
             "Model",
             "Order",
             "MAF",
-            "Heritability",
+            f"{prev_or_her_str}",
             "Error",
             f"Time (s) avg. {_TEST_REPETITIONS}",
         ]
@@ -90,7 +103,7 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
             model_order = int(model_name.split("_")[1])
             for maf in mafs:
                 maf = [maf] * model_order
-                for heritability in heritabilities:
+                for prev_or_her in prevs_or_hers:
                     # Generate model
                     model = pytoxo.model.Model(
                         os.path.join("models", f"{model_name}.csv")
@@ -101,7 +114,7 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
                     for _ in range(_TEST_REPETITIONS):
                         t0 = time.time()
                         # Full penetrance table process since model generation
-                        ptable_sol = model.find_max_prevalence_table(maf, heritability)
+                        ptable_sol = table_method(model, maf, prev_or_her)
                         tf = time.time()
                         computation_times.append(tf - t0)
 
@@ -113,17 +126,15 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
                     """Use the obtained penetrance values to run a second 
                     calculation of the left hand side of the first equation 
                     of the equation system generated in
-                    `find_max_prevalence_table`. This time, the input is 
-                    formed by numerical values, so the final solution will be
-                    numerical and not symbolic."""
-                    recalculated_heritability = (
-                        pytoxo.calculations.compute_heritability(
-                            ptable_sol._penetrance_values, maf, model_order=model.order
-                        )
+                    `find_max_heritability_table` or `find_max_prevalence_table`.
+                    This time, the input is formed by numerical values, 
+                    so the final solution will be numerical and not symbolic."""
+                    recalculated_prev_or_her = recalc_method(
+                        ptable_sol._penetrance_values, maf, model_order=model.order
                     )
 
-                    # Compare recalculated and initial prevalences
-                    delta = heritability - recalculated_heritability
+                    # Compare recalculated and initial prevalence or heritability
+                    delta = prev_or_her - recalculated_prev_or_her
 
                     # Append to delta list for the automatic checks
                     deltas.append(delta)
@@ -134,7 +145,7 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
                             model_name.split("_")[0].capitalize(),
                             model_order,
                             maf[0],
-                            heritability,
+                            prev_or_her,
                             delta,
                             f"{round(computation_time_av, 4)}",
                         ]
@@ -150,7 +161,7 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
             "test",
             "accuracy",
             "reports",
-            f"{test_name}_{now}.tex",
+            f"{test_name}_max_{prev_or_her_letter}_{now}.tex",
         )
         final_table = tabulate.tabulate(
             table_content, headers="firstrow", tablefmt="latex"
@@ -185,7 +196,7 @@ class PenetrancesAccuracyMaxPTestSuite(unittest.TestCase):
                 f"{final_table}"
                 "\n"
                 "\\caption{Accuracies of the the calculated values for the "
-                "\\penetrances. Maximizing prevalence}\n"
+                f"\\penetrances. Maximizing {prev_or_her_str.lower()}}}\n"
                 "\\end{figure}\n"
                 f"Datetime: {now_tex}\n\n"
                 f"Machine: \\texttt{{{machine_info_tex}}}\n\n"
