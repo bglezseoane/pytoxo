3e38bad3d3e09c5b215ab929b3632ba985eec3d6
Borja González Seoane <borja.gseoane@udc.es>
2021-04-16 17:32:48 +0200

    Create GUI package

---
+++ pytoxo_gui/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+# PyToxo
+#
+# A Python library for calculating penetrance tables of any
+# bivariate epistasis model.
+#
+# Copyright 2021 Borja González Seoane
+#
+# Contact: borja.gseoane@udc.es
+###########################################################
+
+"""PyToxo graphical user interface package."""
