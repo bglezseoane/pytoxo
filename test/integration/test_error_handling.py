e8a34a555b46c6b269ba724994d145ab5c899af8
Borja González Seoane <borja.gseoane@udc.es>
2021-05-12 17:34:23 +0200

    Fix test with actually unsolvable cases

--- test/integration/test_error_handling.py
+++ test/integration/test_error_handling.py
@@ -31,15 +31,12 @@ class ErrorHandlingTestSuite(unittest.TestCase):
     def test_exception_raising_during_models_solving(self):
         """Test exception raising and catching during models solving using some
         known unsolvable cases."""
-        # Cases determined unsolvable by Toxo
+        # Cases known as unsolvable
         cases = [
-            (os.path.join("models", "additive_4.csv"), 0.3, 0.1),
-            (os.path.join("models", "additive_5.csv"), 0.3, 0.1),
-            (os.path.join("models", "additive_6.csv"), 0.49, 0.1),
-            (os.path.join("models", "additive_6.csv"), 0.49, 0.2),
-            (os.path.join("models", "multiplicative_2.csv"), 0.1, 0.1),
-            (os.path.join("models", "multiplicative_2.csv"), 0.2, 0.1),
-            (os.path.join("models", "multiplicative_2.csv"), 0.3456, 0.2),
+            (os.path.join("models", "additive_4.csv"), 0.5, 0.0),
+            (os.path.join("models", "additive_5.csv"), 0.4, 1.0),
+            (os.path.join("models", "additive_6.csv"), 0.3, 1.0),
+            (os.path.join("models", "additive_6.csv"), 0.2, 1.0),
         ]
 
         for case_file, case_mafs, case_p in cases:
