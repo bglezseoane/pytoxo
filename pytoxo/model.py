b1d1aee01f0631ffcccad8313a920c20561a1e78
Borja González Seoane <borja.gseoane@udc.es>
2021-06-03 12:52:55 +0200

    Fix comment

--- pytoxo/model.py
+++ pytoxo/model.py
@@ -582,7 +582,7 @@ class Model:
         """
         # Check timeout
         if not solve_timeout:
-            solve_timeout = None  # `timeout_decorator` requires exactly `None`
+            solve_timeout = None  # Required exactly `None`
         elif type(solve_timeout) == bool and solve_timeout == True:
             solve_timeout = (self._order + 1) ** 2 * 60  # Seconds
 
