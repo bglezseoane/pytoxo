b11ba79426424c33f397701c70ad3343ec5aa42c
Borja González Seoane <borja.gseoane@udc.es>
2022-02-12 01:45:16 +0100

    Improve merge CSV script to add solubility contribution

--- test/accuracy/merge_accuracy_csv.py
+++ test/accuracy/merge_accuracy_csv.py
@@ -18,6 +18,11 @@
 Script to merge CSV outputs of scripts `test_variables_accuracy.py` and
 `build_toxo_accuracy_contrast.py` to achieve contrast data between PyToxo and
 Toxo.
+
+Enabling the flag `SOLUBILITY_CONTRIBUTION` this test also prepares a
+contribution report for the solubility collection, taking advantage of this
+data. This is only interesting handling reports (`PYTOXO_REPORT_FILENAME` and
+`TOXO_REPORT_FILENAME`) with a big amount of cases within.
 """
 
 import datetime
@@ -30,6 +35,7 @@ import pandas as pd  # TODO: Add to requirements
 PYTOXO_REPORT_FILENAME = "test_variables_accuracy_2022-02-08_03-31-41.csv"
 TOXO_REPORT_FILENAME = "build_toxo_accuracy_contrast_2022-02-08_17-51-47.csv"
 SAVE = True
+SOLUBILITY_CONTRIBUTION = True
 # #########################################################
 
 # Assert project home directory because sources have path since here
@@ -103,3 +109,70 @@ if SAVE:
         sep=";",
         mode="a",
     )
+
+
+if SOLUBILITY_CONTRIBUTION:
+    # Merge reports again, but preserving cases only solvable by Pytoxo or by
+    # Toxo
+    merged = pytoxo_report.merge(
+        toxo_report,
+        on=["Model", "Order", "MAF", "Heritability"],
+        how="left",
+        indicator=True,
+    )
+
+    # Achieve masks: only-Pytoxo, only_Toxo, both
+    only_pytoxo_mask = merged["_merge"] == "left_only"
+    both_mask = merged["_merge"] == "both"
+    only_toxo_mask = merged["_merge"] == "right_only"
+
+    # Prepare solubility contribution aux. data frames
+    only_pytoxo = merged[only_pytoxo_mask].drop("_merge", 1)  # Drop useless columns
+    both = merged[both_mask].drop("_merge", 1)  # Drop useless columns
+    only_toxo = merged[only_toxo_mask].drop("_merge", 1)  # Drop useless columns
+
+    if SAVE:
+        # Prepare output paths
+        contribution_report_base_path = os.path.join(
+            "test",
+            "solubility",
+            "reports",
+        )
+        contribution_report_paths = [
+            os.path.join(
+                contribution_report_base_path, f"{script_name}_only_pytoxo_{now}.csv"
+            ),
+            os.path.join(
+                contribution_report_base_path, f"{script_name}_both_{now}.csv"
+            ),
+            os.path.join(
+                contribution_report_base_path, f"{script_name}_only_toxo_{now}.csv"
+            ),
+        ]
+
+        # Save contribution data frames as CSV
+        for filename, dataframe in zip(
+            contribution_report_paths, [only_pytoxo, both, only_toxo]
+        ):
+            with open(filename, "w") as f:  # Patch to set custom headers
+                f.write(
+                    ";".join(
+                        [
+                            "Model",
+                            "Order",
+                            "MAF",
+                            "Heritability",
+                            "Error PyToxo",
+                            "Time (s) avg. 3 PyToxo",
+                            "Error Toxo",
+                            "Time (s) avg. Toxo\n",
+                        ]
+                    )
+                )
+            dataframe.to_csv(
+                filename,
+                header=False,
+                index=False,
+                sep=";",
+                mode="a",
+            )
